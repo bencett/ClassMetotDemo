fe552541b20c3645b09d74f8a1ac0e2cd3306096
Çetin Ürün <cetinurun.12@outlook.com>
2021-03-09 21:52:05 +0300

    Day 3 practice

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerId + customer.CustomerName + customer.CustomerSurname + " Eklendi");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerId + customer.CustomerName + customer.CustomerSurname + " Silindi");
+        }
+
+        public void CustomerList(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerId + " " + customer.CustomerName + " " + customer.CustomerSurname + " Listelendi");
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,29 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 1;
+            customer1.CustomerName = "Çetin";
+            customer1.CustomerSurname = "Ürün";
+
+            Customer customer2 = new Customer();
+            customer2.CustomerId = 2;
+            customer2.CustomerName = "Vedat";
+            customer2.CustomerSurname = "Kaya";
+
+            Customer customer3 = new Customer();
+            customer3.CustomerId = 3;
+            customer3.CustomerName = "Hacı Ahmet";
+            customer3.CustomerSurname = "Karadağ";
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer2);
+            customerManager.Delete(customer3);
+            customerManager.CustomerList(customer1);
+            
+
         }
     }
 }
